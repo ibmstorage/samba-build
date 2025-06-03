ecd069a731b6d1c438ad8d19bbd1492e235bb3c4
Anoop C S <anoopcs@samba.org>
2025-06-03 16:07:50 +0530

    build.rpms: Add support for custom vendor dist suffix
    Like %{dist}, 'vendor_dist_suffix' environment variable is supposed
    to contain the required delimiter i.e, dot('.') to get it meaningfully
    added to the 'Release' tag.

--- ansible/roles/build.rpms.rhel/files/rpm-build.sh
+++ ansible/roles/build.rpms.rhel/files/rpm-build.sh
@@ -8,6 +8,10 @@ dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VE
 
 dnf -y install mock rpm-build
 
-mock --root "${MOCK_DIR}/rhel-${OS_VERS}-${OS_ARCH}.cfg" --resultdir "${RPMS_DIR}/${SAMBA_VERS}/rhel/${OS_VERS}/${OS_ARCH}" --rebuild "${SRPM_DIR}/${SAMBA_SRPM}"
+if [ -z "$VENDOR_DIST_SUFFIX" ]; then
+	mock --root "${MOCK_DIR}/rhel-${OS_VERS}-${OS_ARCH}.cfg" --resultdir "${RPMS_DIR}/${SAMBA_VERS}/rhel/${OS_VERS}/${OS_ARCH}" --rebuild "${SRPM_DIR}/${SAMBA_SRPM}"
+else
+	mock --root "${MOCK_DIR}/rhel-${OS_VERS}-${OS_ARCH}.cfg" --resultdir "${RPMS_DIR}/${SAMBA_VERS}/rhel/${OS_VERS}/${OS_ARCH}" --define "vendor_dist ${VENDOR_DIST_SUFFIX}" --rebuild "${SRPM_DIR}/${SAMBA_SRPM}"
+fi
 
 subscription-manager unregister
